7ad052ba7f7466e99a4ca2aae7eeea159444ab4c
Beck Qin <enginebeck@gmail.com>
2020-09-29 12:42:08 +0800

    finished add two numbers but wrong

--- medium/addtwonumbers.go
+++ medium/addtwonumbers.go
@@ -3,6 +3,7 @@ package medium
 import (
 	"fmt"
 	"math"
+	"strconv"
 )
 
 /**
@@ -18,22 +19,23 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	//      2. convert to integer
 	//      3. sum
 	//      4. split and reverse
-	l1RevArr := listNode2Array(l1)
-	l2RevArr := listNode2Array(l2)
-	maxLen := len(l1RevArr)
-	if len(l2RevArr) > maxLen {
-		maxLen = len(l2RevArr)
+	l1Arr := listNode2Array(l1)
+	l2Arr := listNode2Array(l2)
+	maxLen := len(l1Arr)
+	if len(l2Arr) > maxLen {
+		maxLen = len(l2Arr)
 	}
-	l1number := getNumber(l1RevArr, maxLen)
-	l2number := getNumber(l2RevArr, maxLen)
+	l1number := getNumber(l1Arr, maxLen)
+	l2number := getNumber(l2Arr, maxLen)
 	sumResult := l1number + l2number
-	fmt.Println(sumResult)
-
-	return getResult(sumResult, maxLen)
+	fmt.Println(l1Arr, l2Arr)
+	fmt.Println(l1number, l2number)
+	return getResult(sumResult)
 }
 
-func getResult(sumResult int, maxLen int) *ListNode {
+func getResult(sumResult int) *ListNode {
 	var result *ListNode
+	maxLen := len(strconv.Itoa(sumResult))
 	for index := 0; index < maxLen; {
 		result = &ListNode{sumResult / int(math.Pow10(maxLen-1-index)), result}
 		sumResult = sumResult % int(math.Pow10(maxLen-1-index))
@@ -44,9 +46,12 @@ func getResult(sumResult int, maxLen int) *ListNode {
 
 func getNumber(arr []int, maxLen int) int {
 	result := 0
-	for index := 0; index < maxLen; {
-		result += arr[index] * int(math.Pow10(maxLen-index-1))
-		index++
+	for index := maxLen - 1; index >= 0; {
+		if index >= len(arr) {
+			index = len(arr) - 1
+		}
+		result += arr[index] * int(math.Pow10(index))
+		index--
 	}
 	return result
 }
@@ -56,6 +61,18 @@ func listNode2Array(node *ListNode) []int {
 	for node != nil {
 		result = append(result, node.Val)
 		node = node.Next
+	}
+	return result
+}
+
+func intArray2ListNode(arr []int) *ListNode {
+	var result *ListNode
+	for index := len(arr) - 1; index >= 0; {
+		result = &ListNode{
+			Val:  arr[index],
+			Next: result,
+		}
+		index--
 	}
 	return result
 }
--- medium/addtwonumbers_test.go
+++ medium/addtwonumbers_test.go
@@ -1,36 +1,32 @@
 package medium
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAddTwoNumbers(t *testing.T) {
-	param1 := &ListNode{
-		Val: 2,
-		Next: &ListNode{
-			Val: 4,
-			Next: &ListNode{
-				Val:  3,
-				Next: nil,
-			},
-		},
+	cases := []struct {
+		Param1 *ListNode
+		Param2 *ListNode
+		Expect []int
+	}{{
+		intArray2ListNode([]int{2, 4, 3}),
+		intArray2ListNode([]int{5, 6, 4}),
+		[]int{7, 0, 8},
+	}, {
+		intArray2ListNode([]int{5}),
+		intArray2ListNode([]int{5}),
+		[]int{0, 1},
+	}, {
+		intArray2ListNode([]int{1, 8}),
+		intArray2ListNode([]int{0}),
+		[]int{1, 8},
+	}}
+	for _, caseItem := range cases {
+		result := addTwoNumbers(caseItem.Param1, caseItem.Param2)
+		resultArray := listNode2Array(result)
+		assert.Equal(t, caseItem.Expect, resultArray)
 	}
-	param2 := &ListNode{
-		Val: 5,
-		Next: &ListNode{
-			Val: 6,
-			Next: &ListNode{
-				Val:  4,
-				Next: nil,
-			},
-		},
-	}
-	expect := []int{7, 0, 8}
-	result := addTwoNumbers(param1, param2)
-	resultArray := listNode2Array(result)
-	fmt.Println(resultArray)
-	assert.Equal(t, expect, resultArray)
 }
