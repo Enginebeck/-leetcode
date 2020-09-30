a5d93d7e6dcb6ca469bcbc2a9bd189d86b39ad6b
Beck Qin <enginebeck@gmail.com>
2020-09-30 16:02:32 +0800

    solved 7

---
+++ simple/7.go
@@ -0,0 +1,28 @@
+package simple
+
+import (
+	"math"
+)
+
+func reverse(x int) int {
+	max := int64(math.Abs(math.Pow(float64(2), 31))) - 1
+	min := -1 * (max + 1)
+	if x == 0 {
+		return 0
+	}
+
+	absX := int(math.Abs(float64(x)))
+
+	var result int64
+	for index := 0; absX >= 1; index++ {
+		if result*10+int64(absX%10) > max || result*10+int64(absX%10)*-1 < min {
+			return 0
+		} else {
+			result = result*10 + int64(absX%10)
+			absX = absX / 10
+		}
+	}
+	result = result * int64(x) / int64(math.Abs(float64(x)))
+
+	return int(result)
+}
---
+++ simple/7_test.go
@@ -0,0 +1,31 @@
+package simple
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReverse(t *testing.T) {
+	cases := []struct {
+		Input  int
+		Expect int
+	}{
+		//{
+		//	Input:  123,
+		//	Expect: 321,
+		//},
+		//{
+		//	Input:  -1,
+		//	Expect: -1,
+		//},
+		{
+			Input:  1534236469,
+			Expect: 0,
+		},
+	}
+	for _, testCase := range cases {
+		result := reverse(testCase.Input)
+		assert.Equal(t, testCase.Expect, result)
+	}
+}
