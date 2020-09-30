5ea2d08768313d67c7752384810966d5d79e2f2e
Beck Qin <enginebeck@gmail.com>
2020-09-30 13:00:07 +0800

    remove log

--- simple/1480.go
+++ simple/1480.go
@@ -1,13 +1,10 @@
 package simple
 
-import "fmt"
-
 func runningSum(nums []int) []int {
 	for index, _ := range nums {
 		if index > 0 {
 			nums[index] += nums[index-1]
 		}
 	}
-	fmt.Println(nums)
 	return nums
 }
