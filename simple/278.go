9ef8246b0ff846b42d769deede95266b74e7a0fe
Beck Qin <enginebeck@gmail.com>
2020-09-30 16:16:08 +0800

    start 278

---
+++ simple/278.go
@@ -0,0 +1,17 @@
+package simple
+
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+
+}
+
+func isBadVersion(version int) bool {
+	return false
+}
