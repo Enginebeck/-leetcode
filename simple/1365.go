b6c59c58aec777e764013009ceafb8d27d41c55f
Beck Qin <enginebeck@gmail.com>
2020-10-26 23:28:11 +0800

    change var declar to mkae

--- simple/1365.go
+++ simple/1365.go
@@ -2,10 +2,7 @@ package simple
 
 func smallerNumbersThanCurrent(nums []int) []int {
 	numsLen := len(nums)
-	var result []int
-	for index := 0; index < numsLen; index++ {
-		result = append(result, 0)
-	}
+	result := make([]int, numsLen)
 	for index, item := range nums {
 		for subIndex := index + 1; subIndex < numsLen; subIndex++ {
 			if item > nums[subIndex] {
